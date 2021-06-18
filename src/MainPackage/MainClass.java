bc876332792fd56e7ca45101e11bab6af3586986
imzhyke <punk1313444@gmail.com>
2021-06-19 00:12:59 +0800

    Update

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,9 +1,12 @@
     
 package MainPackage;
 
+
+
 public class MainClass {
     
     public static void main(String[] args){
+
         IntroFrame intro = new IntroFrame();
         intro.setVisible(true);
     }
